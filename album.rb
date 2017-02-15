de99765ed6f39913cac5cb370ab36e1927ee1b8d
Robert Ball <robert.ball.1989@gmail.com>
2017-02-15 15:33:55 +0000

    Album save function works.

--- album.rb
+++ album.rb
@@ -1,11 +1,27 @@
 require_relative('./sql_runner')
 
 class Album
-
+  attr_reader :id, :title, :genre, :artist_id
   def initialize(options)
     @title = options['title']
     @genre = options['genre']
     @artist_id = options['artist_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO albums 
+    (title,genre,artist_id) 
+    VALUES 
+    (
+    '#{@title}',
+    '#{@genre}',
+    '#{@artist_id}') 
+    RETURNING *;
+    "
+    savedalbums = SqlRunner.run(sql)
+    album_object = savedalbums.map {|album| Album.new(album)}
+    id_string = album_object[0].id
+    @id = id_string.to_i
   end
 
 end
--- console.rb
+++ console.rb
@@ -6,12 +6,12 @@ artist1 = Artist.new({'name' => 'R Kelly'})
 artist1.save()
 
 
-# album1 = Album.new({'title' => 'Ignition', 'genre' => 'RnB', 'artist_id' => artist1.id})
-# album1.save()
+ album1 = Album.new({'title' => 'Ignition', 'genre' => 'RnB', 'artist_id' => artist1.id})
+ album1.save()
 
 
-# album2 = Album.new({'title' => 'Trapped in the Closet', 'genre' => 'RnB', 'artist_id' => artist1.id})
-# album2.save()
+ album2 = Album.new({'title' => 'Trapped in the Closet', 'genre' => 'RnB', 'artist_id' => artist1.id})
+ album2.save()
 
 
 
