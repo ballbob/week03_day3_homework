c284aa8507c9758a844fedda34f3e04677ef46ab
Robert Ball <robert.ball.1989@gmail.com>
2017-02-15 15:36:00 +0000

    List_all for artist works.

--- artist.rb
+++ artist.rb
@@ -14,14 +14,16 @@ class Artist
 
     sql = "INSERT INTO artists (name) VALUES ('#{@name}') RETURNING *;"
     saved_artists = SqlRunner.run(sql)
-
     artist_object = saved_artists.map {|artist| Artist.new(artist)}
-
     id_string = artist_object[0].id
-
     @id = id_string.to_i
+  end
 
-
+  def self.list_all
+    sql = "SELECT * FROM artists;"
+    list_hash = SqlRunner.run(sql)
+    list_object = list_hash.map { |artist| Artist.new(artist)}
+    return list_object
   end
 
 
